e910143d04d933047a7dfad0b60688620cbbfec9
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-11-07 12:54:39 +0100

    add main, btn and card whit p

---
+++ src/components/Main.jsx
@@ -0,0 +1,36 @@
+export default function Main() {
+
+
+    return (
+
+        <main className="container">
+            <div className="d-flex">
+                <button className="btn btn-primary">
+                    <h3>HTML</h3>
+                </button>
+                <button className="btn btn-primary">
+                    <h3>CSS</h3>
+                </button>
+                <button className="btn btn-primary">
+                    <h3>JavaScript</h3>
+                </button>
+                <button className="btn btn-primary">
+                    <h3>Node.js</h3>
+                </button>
+                <button className="btn btn-primary">
+                    <h3>Express</h3>
+                </button>
+                <button className="btn btn-primary">
+                    <h3>ReactJs</h3>
+                </button>
+            </div>
+
+            <div class="card my-5">
+                <div class="card-body">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+                </div>
+            </div>
+        </main>
+
+    )
+}
